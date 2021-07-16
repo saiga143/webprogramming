061bca813a0bec091eb3111c9887e997138eb98a
Sai Ganesh Veeravalli <saiganeshveeravalli@gmail.com>
2021-07-16 13:14:46 +0200

    added conditions file

---
+++ python/conditions.py
@@ -0,0 +1,11 @@
+a  = input("Number: ")
+a = int(a)
+
+if a > 0:
+    print("Your number", a, "is positive")
+
+elif a <0:
+    print("Your number", a, "is negative")
+
+else:
+    print("Your number is zero")
