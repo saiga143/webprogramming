f220203b58ecfe0daf77d6d4c9131e1fbe02b1d1
Sai Ganesh Veeravalli <saiganeshveeravalli@gmail.com>
2021-07-16 13:26:16 +0200

    created lists file

---
+++ python/lists.py
@@ -0,0 +1,14 @@
+#list of names
+name_list = ["Sai", "Ganesh", "Veeravalli"]
+
+print(name_list)
+
+#add a new name to the list
+name_list.append("Daniel")
+
+print(name_list)
+
+#sort the names list in alphabetical order
+name_list.sort()
+
+print(name_list)
